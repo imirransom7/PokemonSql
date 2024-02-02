9afa3571122192581488bf2bd5da6b8335f8e7b8
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-02-02 15:26:01 -0500

    completed part 2 from the README.md

--- part2.sql
+++ part2.sql
@@ -1,11 +1,25 @@
 -- 1:
 SELECT name
 FROM types;
--- OR
-SELECT COUNT(*)
-FROM types;
 
 -- 2:
 SELECT name
 FROM pokemons
 WHERE id = 45;
+
+-- 3:
+SELECT COUNT(*)
+FROM pokemons;
+
+-- 4
+SELECT COUNT(*)
+FROM types;
+
+--5
+SELECT COUNT(secondary_type)
+FROM pokemons;
+
+--  A better solution would be this one
+SELECT COUNT(secondary_type)
+FROM pokemons
+WHERE secondary_types IS NOT NULL;
